cc1b417731b0084a7b4703bf59183e1ff709a1a5
Ashakuur1 <engshakrayare114@gmail.com>
2024-11-11 15:07:48 +0300

    Add multiple section components to Home page for enhanced content display

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,24 @@
 import Image from "next/image";
 import HeroSection from "./components/Hero-section";
 import About from "./components/About";
+import Services from "./components/Services-section";
+import Portfolio from "./components/Portfolio-section";
+import CompanySnapshot from "./components/Company-snapshot-section";
+import Testimonials from "./components/Testimonials-section";
+import Customers from "./components/Customers-section";
+import CallToAction from "./components/Cta-section";
 
 export default function Home() {
   return (
     <div>
       <HeroSection />
       <About />
+      <Services />
+      <Portfolio />
+      <CompanySnapshot />
+      <Testimonials />
+      <Customers />
+      <CallToAction />
     </div>
   );
 }
